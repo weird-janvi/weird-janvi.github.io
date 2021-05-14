71767c13fe0badd33f7f0d90eba897a3ce3cfb9b
janvi7109 <Janvi.Chhabra@iiitb.org>
2021-05-14 11:09:55 +0530

    2

--- Documents/SEM6/Computer_Graphics/project/code/trial/dist/script.js
+++ Documents/SEM6/Computer_Graphics/project/code/trial/dist/script.js
@@ -3,36 +3,73 @@ var scene;
 var camera;
 var mesh;
 var cube;
+var collisions = [];
 init();
+pick();
+
 animate();
 
 function animate() {
 	
 	requestAnimationFrame ( animate );
-	window.addEventListener("keydown", function(eee){
-		if(eee.keyCode==38)
-		{
-			cube.translateX(0.01);
-			console.log("pos", cube.position);
-		}
-	});
-
+	
+	moveRight(mesh);
+	moveLeft(cube);
+	moveUp(cube);
+	moveDown(cube);
+	
 	mesh.rotation.x += 0.00;
 	mesh.rotation.y += 0.00;
 	// cube.rotation.x += 0.00;
 	// cube.rotation.y += 0.00;
 	// cube.position.x = 300;
 	// cube.position.y = 200;
-	mesh.position.x = -100;
-	mesh.position.y = 200;
+	
 
 	//Render the scene
 	renderer.render(scene, camera);
 }
-function moveup(obj)
+function moveRight(obj)
 {
-	obj.position.y += 100;
+	window.addEventListener("keydown", function(eee){
+		if(eee.keyCode==39)
+		{
+			obj.translateX(0.01);
+			console.log("pos", obj.position);
+		}
+	});
 }
+function moveLeft(obj)
+{
+	window.addEventListener("keydown", function(eee){
+		if(eee.keyCode==37)
+		{
+			obj.translateX(-0.01);
+			console.log("pos", obj.position);
+		}
+	});
+}
+function moveUp(obj)
+{
+	window.addEventListener("keydown", function(eee){
+		if(eee.keyCode==38)
+		{
+			obj.translateY(0.01);
+			console.log("pos", obj.position);
+		}
+	});
+}
+function moveDown(obj)
+{
+	window.addEventListener("keydown", function(eee){
+		if(eee.keyCode==40)
+		{
+			obj.translateY(-0.01);
+			console.log("pos", obj.position);
+		}
+	});
+}
+
 function init() {
 	
 	renderer = new THREE.WebGLRenderer();
@@ -68,6 +105,8 @@ function init() {
 	scene.add(cube);
 	//Add the sphere
 	mesh = new THREE.Mesh(geometry, appearence);
+	mesh.position.x = -100;
+	mesh.position.y = 200;
 	scene.add(mesh);
 	
 	
@@ -78,3 +117,72 @@ function init() {
 	floor.rotation.x = 90;
 	scene.add( floor ); 
 }
+
+function detectCollisions() {
+	// Get the user's current collision area.
+	var bounds = {
+	  xMin: rotationPoint.position.x - box.geometry.parameters.width / 2,
+	  xMax: rotationPoint.position.x + box.geometry.parameters.width / 2,
+	  yMin: rotationPoint.position.y - box.geometry.parameters.height / 2,
+	  yMax: rotationPoint.position.y + box.geometry.parameters.height / 2,
+	  zMin: rotationPoint.position.z - box.geometry.parameters.width / 2,
+	  zMax: rotationPoint.position.z + box.geometry.parameters.width / 2,
+	};
+	
+	// Run through each object and detect if there is a collision.
+	for ( var index = 0; index < collisions.length; index ++ ) {
+  
+	  if (collisions[ index ].type == 'collision' ) {
+		if ( ( bounds.xMin <= collisions[ index ].xMax && bounds.xMax >= collisions[ index ].xMin ) &&
+		   ( bounds.yMin <= collisions[ index ].yMax && bounds.yMax >= collisions[ index ].yMin) &&
+		   ( bounds.zMin <= collisions[ index ].zMax && bounds.zMax >= collisions[ index ].zMin) ) {
+		  // We hit a solid object! Stop all movements.
+		  stopMovement();
+  
+		  // Move the object in the clear. Detect the best direction to move.
+		  if ( bounds.xMin <= collisions[ index ].xMax && bounds.xMax >= collisions[ index ].xMin ) {
+			// Determine center then push out accordingly.
+			var objectCenterX = ((collisions[ index ].xMax - collisions[ index ].xMin) / 2) + collisions[ index ].xMin;
+			var playerCenterX = ((bounds.xMax - bounds.xMin) / 2) + bounds.xMin;
+			var objectCenterZ = ((collisions[ index ].zMax - collisions[ index ].zMin) / 2) + collisions[ index ].zMin;
+			var playerCenterZ = ((bounds.zMax - bounds.zMin) / 2) + bounds.zMin;
+  
+			// Determine the X axis push.
+			if (objectCenterX > playerCenterX) {
+			  rotationPoint.position.x -= 1;
+			} else {
+			  rotationPoint.position.x += 1;
+			}
+		  }
+		  if ( bounds.zMin <= collisions[ index ].zMax && bounds.zMax >= collisions[ index ].zMin ) {
+			// Determine the Z axis push.
+			if (objectCenterZ > playerCenterZ) {
+			rotationPoint.position.z -= 1;
+			} else {
+			  rotationPoint.position.z += 1;
+			}
+		  }
+		}
+	  }
+	}
+  }
+  
+  /**
+   * Calculates collision detection parameters.
+   */
+  function calculateCollisionPoints( mesh, scale, type = 'collision' ) { 
+	// Compute the bounding box after scale, translation, etc.
+	var bbox = new THREE.Box3().setFromObject(mesh);
+   
+	var bounds = {
+	  type: type,
+	  xMin: bbox.min.x,
+	  xMax: bbox.max.x,
+	  yMin: bbox.min.y,
+	  yMax: bbox.max.y,
+	  zMin: bbox.min.z,
+	  zMax: bbox.max.z,
+	};
+	
+	collisions.push( bounds );
+  }
